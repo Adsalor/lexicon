7d4c723aeb6f634ef3ca9020a24847fb59dad724
abank <banka@rpi.edu>
2023-07-05 00:47:40 -0400

    moved tile class to inputDevices

--- src/inputDevices.js
+++ src/inputDevices.js
@@ -32,6 +32,10 @@ class InputDevice {
 
 class Button extends InputDevice {
     //...
+    constructor(){
+        super();
+        this.selected=false;
+    }
 }
 
 class Switch extends InputDevice {
@@ -45,4 +49,71 @@ class Slider extends InputDevice {
 
 class Tile extends Button {
     //...
+    constructor(letter, x, y) {
+        super();
+        this.letter = letter;
+        this.x = x;
+        this.y = y;
+    }
+
+    //hexagon with letter (or capital)
+    renderFull(canvas){
+        const context = canvas.getContext('2d');
+        const size = 50; // Size of the hexagon
+        const centerX = this.x;
+        const centerY = this.y;
+        // Draws the hexagon outline
+        context.beginPath();
+        context.moveTo(centerX + size * Math.cos(0), centerY + size * Math.sin(0));
+  
+        for (let i = 1; i <= 6; i++) {
+            const angle = (Math.PI / 3) * i;
+            const x = centerX + size * Math.cos(angle);
+            const y = centerY + size * Math.sin(angle);
+            context.lineTo(x, y);
+        }
+  
+        context.closePath();
+  
+        // Fill the hexagon with a color based on the selected state
+        context.fillStyle = this.selected ? 'black' : 'white';
+        context.fill();
+  
+        // Add the letter in the middle of the hexagon
+        context.fillStyle = this.selected ? 'white' : 'black';
+        context.font = 'bold 20px Arial';
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        context.fillText(this.letter, centerX, centerY);
+    }
+    //hexagon without letter
+    renderEmpty(canvas){
+        const context = canvas.getContext('2d');
+        const size = 25; // Size of the hexagon
+        const centerX = this.x;
+        const centerY = this.y;
+        // Draws the hexagon outline
+        context.beginPath();
+        context.moveTo(centerX + size * Math.cos(0), centerY + size * Math.sin(0));
+        context.lineWidth=5;
+        context.strokeStyle='black';
+        for (let i = 1; i <= 6; i++) {
+            const angle = (Math.PI / 3) * i;
+            const x = centerX + size * Math.cos(angle);
+            const y = centerY + size * Math.sin(angle);
+            context.lineTo(x, y);
+        }
+  
+        context.closePath();
+        context.stroke();
+    }
+
+    render(canvas) {
+        if(this.letter==''){
+            this.renderEmpty(canvas);
+        }else{
+            this.renderFull(canvas);
+        }
+        
+    }
 }
