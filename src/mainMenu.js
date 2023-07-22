6b448a28fc134c39c92598ae0eeb03b957a2dfb8
Alex Xie <45025485+alexxie2288@users.noreply.github.com>
2023-07-22 14:32:04 -0400

    main menu

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -1,3 +1,36 @@
-mainButtons = [];
+class mainMenu {
+    constructor(menuItems) {
+      this.menuItems = menuItems;
+    }
+  
+    createMenu() {
+      const mainMenuElement = document.getElementById('mainMenu');
+  
+      this.menuItems.forEach((item) => {
+        const button = document.createElement('button');
+        button.textContent = item.label;
+        button.addEventListener('click', () => this.handleItemClick(item.url));
+        mainMenuElement.appendChild(button);
+      });
+    }
+  
+    handleItemClick(url) {
+      // Add your logic here to handle the button click, e.g., navigation to the URL
+      console.log(`Navigating to: ${url}`);
+      alert('Oh, you clicked me!')
+    }
+  }
 
-var mainMenu = new Menu();
+    const menuItems = [
+        { label: 'SinglePlayer', url: '#' },
+        { label: 'Multiplayer', url: '#' },
+        { label: 'Settings', url: '#' },
+      ];
+      
+      const menu = new mainMenu(menuItems);
+      menu.createMenu();
+    // var button1 = new Button(false,1,16/9,0.1);
+    // button1.render($("renderTarget#Menu"));
+
+
+
